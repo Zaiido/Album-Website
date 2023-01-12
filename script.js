37fd804f8bc8813e2416e7ec4911b727b2174ca0
Zaide Kurti <zaiido1920@gmail.com>
2023-01-12 16:55:36 +0100

    Added animations

--- script.js
+++ script.js
@@ -5,4 +5,32 @@ const albumNumbers = function () {
   ).innerHTML = `Number of albums: ${albumNodes.length}`;
 };
 
+const deleteTrack = function () {
+  let buttonNodes = document.querySelectorAll("#tracklist button.btn-danger");
+  for (let button of buttonNodes) {
+    button.addEventListener("click", function (eventData) {
+      eventData.target.parentElement.parentElement.parentElement.removeChild(
+        eventData.target.parentElement.parentElement
+      );
+    });
+  }
+};
+
+const addTrack = function () {
+  let newTrack = document.createElement("tr");
+  newTrack.innerHTML = `
+  <th scope="row">${document.querySelector("#number").value}</th>
+  <td>${document.querySelector("#title").value}</td>
+  <td>${document.querySelector("#artist").value}</td>
+  <td>${document.querySelector("#duration").value}</td>
+  <td><button onclick="deleteTrack()" class="btn btn-danger">Delete</button></td>`;
+
+  document.querySelector("#tracklist tbody").appendChild(newTrack);
+};
+
+document
+  .querySelector(".modal-footer button:last-child")
+  .addEventListener("click", addTrack);
+
 window.onload = albumNumbers;
+window.onload = deleteTrack;
